a7df19de1771ef7677dc814f922478c1be6c6b54
baesparza <bruno.be81@gmail.com>
2017-11-09 14:53:05 -0500

    feat(*): first commit

---
+++ src/principal/Principal.java
@@ -0,0 +1,14 @@
+package principal;
+
+/**
+ *
+ * @author baesparza
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+    }
+    
+}
