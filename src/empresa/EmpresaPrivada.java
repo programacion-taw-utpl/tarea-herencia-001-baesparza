277500022a9eca51023f9fe2f23118adde3228ba
baesparza <bruno.be81@gmail.com>
2017-11-09 18:12:17 -0500

    deat(*): add EmpresaPrivada class

---
+++ src/empresa/EmpresaPrivada.java
@@ -0,0 +1,57 @@
+package empresa;
+
+import persona.Trabajador;
+
+/**
+ *
+ * @author baesparza
+ */
+public class EmpresaPrivada extends Empresa {
+    
+    private Double ventas_mensual_fijo;
+    private int numero_sucursales;
+    
+    public EmpresaPrivada() {
+        super("Nombre empreza indefinido", "n/a", "n/a", new Trabajador[] {});
+        this.setVentas_mensual_fijo("0");
+        this.setNumero_sucursales("0");
+    }
+    
+    public EmpresaPrivada(String nombre, String siglas, String ciudad, Trabajador[] trabajadores) {
+        super(nombre, siglas, ciudad, trabajadores);
+        this.setVentas_mensual_fijo("0");
+        this.setNumero_sucursales("0");
+    }
+    
+    public EmpresaPrivada(String ventas_mensual_fijo, String numero_sucursales) {
+        super("Nombre empreza indefinido", "n/a", "n/a", new Trabajador[] {});
+        this.setVentas_mensual_fijo(ventas_mensual_fijo);
+        this.setNumero_sucursales(numero_sucursales);
+    }
+    
+    public EmpresaPrivada(
+            String nombre, String siglas, String ciudad, Trabajador[] trabajadores,
+            String ventas_mensual_fijo, String numero_sucursales
+    ) {
+        super(nombre, siglas, ciudad, trabajadores);
+        this.setVentas_mensual_fijo(ventas_mensual_fijo);
+        this.setNumero_sucursales(numero_sucursales);
+    }
+    
+    public void setVentas_mensual_fijo(String ventas_mensual_fijo) {
+        this.ventas_mensual_fijo = Double.valueOf(ventas_mensual_fijo);
+    }
+    
+    public void setNumero_sucursales(String numero_sucursales) {
+        this.numero_sucursales = Integer.valueOf(numero_sucursales);
+    }
+    
+    public double getVentas_mensual_fijo() {
+        return this.ventas_mensual_fijo;
+    }
+    
+    public int getNumero_sucursales() {
+        return this.numero_sucursales;
+    }
+
+}
